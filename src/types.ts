71a73bf094d152c84260dd10f9dae240cf08f425
miguel heckler lencina <miguellencina13@gmail.com>
2024-01-22 19:02:08 -0300

    Add valueType

--- src/types.ts
+++ src/types.ts
@@ -9,23 +9,26 @@ interface CountedType<T> {
     count: number | undefined;
     parser: ParserFunction<T>;
     writer: WriterFunction<T>;
+    valueType: "string" | "bigint" | "number";
     [isCountedType]: any;
 }
 type SingleType<T> = (count: number) => CountedType<T>;
 type Type<T> = SingleType<T> & CountedType<T>;
-function createType<T>(parser: ParserFunction<T>, writer: WriterFunction<T>): Type<T> {
+function createType<T>(parser: ParserFunction<T>, writer: WriterFunction<T>, valueType: "string" | "bigint" | "number"): Type<T> {
     function singleTypeFunc(count: number): CountedType<T>{
         assert(count > 0, new RangeError("Count needs to be greather than zero"));
         return {
             count,
             parser,
             writer,
+            valueType,
             [isCountedType]: true
         }
     }
     singleTypeFunc.count = undefined;
     singleTypeFunc.parser = parser;
     singleTypeFunc.writer = writer;
+    singleTypeFunc.valueType = valueType;
     singleTypeFunc[isCountedType] = true;
     return singleTypeFunc;
 }
@@ -34,77 +37,77 @@ export const uint8 = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt8();
 }, function(smartBuf, value){
     smartBuf.writeUInt8(value);
-});
+}, "number");
 export const int8 = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readInt8();
 }, function(smartBuf, value){
     smartBuf.writeInt8(value);
-});
+}, "number");
 export const uint16_be = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt16BE();
 }, function(smartBuf, value){
     smartBuf.writeUInt16BE(value);
-});
+}, "number");
 export const uint16_le = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt16LE();
 }, function(smartBuf, value){
     smartBuf.writeUInt16LE(value);
-});
+}, "number");
 export const int16_le = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readInt16LE();
 }, function(smartBuf, value){
     smartBuf.writeInt16LE(value);
-});
+}, "number");
 export const int16_be = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt16BE();
 }, function(smartBuf, value){
     smartBuf.writeInt16BE(value);
-});
+}, "number");
 export const uint32_be = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt32BE();
 }, function(smartBuf, value){
     smartBuf.writeUInt32BE(value);
-});
+}, "number");
 export const uint32_le = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readUInt32LE();
 }, function(smartBuf, value){
     smartBuf.writeUInt32LE(value);
-});
+}, "number");
 export const int32_le = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readInt32LE();
 }, function(smartBuf, value){
     smartBuf.writeInt32LE(value);
-});
+}, "number");
 export const int32_be = createType((smartBuf: SmartBuffer): number => {
     return smartBuf.readInt32BE();
 }, function(smartBuf, value){
     smartBuf.writeInt32BE(value);
-});
+}, "number");
 export const uint64_be = createType((smartBuf: SmartBuffer): bigint => {
     return smartBuf.readBigUInt64BE();
 }, function(smartBuf, value){
     smartBuf.writeBigUInt64BE(value);
-});
+}, "bigint");
 export const uint64_le = createType((smartBuf: SmartBuffer): bigint => {
     return smartBuf.readBigUInt64LE();
 }, function(smartBuf, value){
     smartBuf.writeBigUInt64LE(value);
-});
+}, "bigint");
 export const int64_le = createType((smartBuf: SmartBuffer): bigint => {
     return smartBuf.readBigInt64LE();
 }, function(smartBuf, value){
     smartBuf.writeBigInt64LE(value);
-});
+}, "bigint");
 export const int64_be = createType((smartBuf: SmartBuffer): bigint => {
     return smartBuf.readBigInt64BE();
 }, function(smartBuf, value){
     smartBuf.writeBigInt64BE(value);
-});
+}, "bigint");
 export const char = createType((smartBuf: SmartBuffer): string => {
     return smartBuf.readString(1);
 }, function(smartBuf, value){
     smartBuf.writeString(value);
-});
+}, "string");
 export { isCountedType };
 
 interface Struct<T extends string | number | bigint>{
