40f8306baff4f022cd7c9b6d092f9575df36ebcb
miguel heckler lencina <miguellencina13@gmail.com>
2024-01-22 17:11:25 -0300

    Make typescript equivalent parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,39 @@
 import { SmartBuffer } from "smart-buffer";
-import type {CountedType, Struct} from "./types";
+import  {isCountedType } from "./types";
+import type { Struct } from "./types";
 
-import {uint8} from "./types"
-
-function parse<T extends string | number | bigint>(struct: Struct<T>, buf: Buffer){
-    const smartBuf = SmartBuffer.fromBuffer(buf);
-    const keys = Object.keys(struct);
-    for(const key of keys){
-        const value = struct[key];
-        if(value.isCountedType === true){ // gore code but works
-            
-        }
+// values is a array that all members is a bigint, number a string but not a mix between those types
+// if count is undefined that means the code should not expect a array coming out of it, instead it should expect an string (in C, a char) | bigint | number
+// or else it will receive a string (in C, a char[]) | (bigint | number)[]
+function aggregate(values: (bigint | number | string)[], count: number | undefined): string | bigint | number | (bigint | number)[]{
+    if(count === undefined){
+        return values[0];
+    }
+    if(typeof(values[0]) === "string"){
+        return values.join("");
+    }else{
+        return values as (bigint | number)[];
     }
 }
+function parse<T extends string | number | bigint>(struct: Struct<T>, buf: Buffer | SmartBuffer){
+    const smartBuf = (buf instanceof SmartBuffer) ? buf : SmartBuffer.fromBuffer(buf);
+    const result = {};
+    for(const key in struct){
+        const type = struct[key];
+        
+        if(isCountedType in type){
+            const values = [];
+            for(let i=0;i<(type.count ?? 1);i++){
+                const value = type.parser(smartBuf);
+                values.push(value);
+            }
+            result[key] = aggregate(values, type.count);
+            continue;
+        }else{
+            result[key] = parse(type, smartBuf);
+        }
+    }
+    return result;
+}
 
-parse({
-    hello: 
-}, Buffer.of(20, 40));
+export default parse;
