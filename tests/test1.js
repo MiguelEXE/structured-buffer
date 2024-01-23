d645da1a2fc780d593d0d80988895ecad2bea466
miguel heckler lencina <miguellencina13@gmail.com>
2024-01-23 13:07:42 -0300

    Remove tests

--- tests/test1.js
+++
@@ -1,6 +0,0 @@
-const lib = require("..");
-
-const result = lib.parser.parse(Buffer.from("abcdef"), {
-    abc: lib.types.uint16_le[3]
-});
-console.log(result);
--- tests/test2.js
+++
@@ -1,8 +0,0 @@
-const lib = require("..");
-
-const result = lib.creator.create({
-    abc: lib.types.uint8[3]
-}, {
-    abc: "abc"
-});
-console.log(result);
